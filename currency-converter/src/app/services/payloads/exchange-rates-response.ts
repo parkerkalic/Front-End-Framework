807fa6f3de62aa03bf5bf0231a1c4ce18f893d77
Parker <evankalic@gmail.com>
2021-08-15 17:16:22 -0500

    first commit

---
+++ currency-converter/src/app/services/payloads/exchange-rates-response.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ExchangeRatesResponse} from './payloads/exchange-rates-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyExchangeService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getRates(base: string): Observable<ExchangeRatesResponse> {
+    return this.http.get<ExchangeRatesResponse>(`https://api.exchangeratesapi.io/latest?base=${base}`);
+  }
+}
